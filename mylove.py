bef13a0036c313dfe1aab3dfa8883988e034e87e
KSR1T <68067743+KSR1T@users.noreply.github.com>
2020-08-05 02:30:36 -0400

    Add files via upload

---
+++ mylove.py
@@ -0,0 +1,54 @@
+import time
+from time import sleep
+B = input ('Enter name love: ')
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+print ('ilove you '+B)
+time.sleep(3)
+A = """
+ ``` `` `` ``  `` `` ``  `` `` `` ``  `` `` ``  `` `` `` ``` `` `` ``  `` `` ``  `` `` ...-::-::/++:
+`` `` `````` `` ````````` `` `````` `` ````````` ````.:+syyhdhyyo/-` `` `````` `` ``./oo/-.-////-.``
+``````````````````````````-:+oosssso/-````````````:shddddddddddddddds.````````````.:+sssyyysoo:`````
+ ``` `` `` ``  `` `` `:ohmdhyyyyhddddddy+-` ````oddddddddddddddddddddms.` `` `-/ossssyyyyhss:``` `` 
+`` `` ``  `` `` ``  /hdhso+++++++hdddddddmy-` `hdddddddddddddddddddddddd.-:+ossssyyyo/:`+o- `` ``  `
+ ``` `` `` ``  `` `sdho++++osyyhdddddddddddd/`sdddddddddddddddddddddddddmyssyyyyo/-``` `. ``` `` `` 
+``````````````````sdy+++shddddddddddddddddddm/mdddddddddddddddddddddddddddys+:.`````````````````````
+`````````````````:mdo+sddddddddddddddddddddddNmddddddddddddddddddddddddddm.`````````````````````````
+ ``` `` `` ``  ``hdddddddddddddddddddddddddddmmddddddddddddddddddddddddddm-` ``  `` `` `` ``` `` `` 
+`` `` ``  `` `` .mdddddddddddddddddddddddddddddddddddddddddddddddddddddddm. `` `` `` ``` `` `` ``  `
+ ``` `` `` ``  `:mdddddddddddddddddddddddddddddddddddddddddddddddddddddddd`` `` ``` `` `` ``` `` `` 
+````````````````:mdddddddddddddddddddddddddddddddddddddddddddddddddddddddo``````````````````````````
+`` `` `````` `` -mddddddddddddddddddddddddddddddddddddddddddddddddddddddd.```` `` ````````` ````````
+ ``` `` `` ``  ``hdddddddddddddddddddddddddddddddddddddddddddddddddddddm- `` ``  `` `` `` ``` `` `` 
+`` `` ``  `` `` `:mdddddddddddddddddddddddddddddddddddddddddddddddddddd:``  `` `` `` ``` `` `` ``  `
+ `````` ``````````omdddddddddddddddddddddddddddddddddddddddddddddddddd:`` `` ````````` ````````` `` 
+```````````````````+ddddddddddddddddddddddddddddddddddddddddddddddddd-``````````````````````````````
+`` `` `````` `` `-/+ymddddddddddddddddddddddddddddddddddddddddddddds``` `````` `` `` ``` `` `` `````
+ ``  `.:+/.``.:+ssyyyhmmdddddddddddddddddddddddddddddddddddddddddd/``  `` `` ``  `` `` `` ``  `` `` 
+``./shmmmmdhyyyyhhhddho/oddddddddddddddddddddddddddddddddddddddms.`` `` ``  `` `` `` ``` `` `` ``  `
+:shdmmdhyyyhhdmdhy+:.`````+hmdddddddddddddddddddddddddddddddddh:````````````````````````````````````
+```+yyyhhddmmms-````````````:sddddddddddddddddddddddddddddddd+``````````````````````````````````````
+`` /hddmmmmmmm- `` ``` `` `` `.+hddddddddddddddddddddddddddo. ``` `` `` ``  `` `` `` ``` `` `` ``` `
+ ```-+mmmmmmms``` `` ``  `` `` ``/dddddddddddddddddddddddo.` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
+`` ``/mmmds:``` `` ``` `` `` ``` ``odddddddddddddddddddh.` `` ``` `` `` `````` `` `` ``` `` `` ``` `
+`````hy+.```````````````````````````.ydddddddddddddddds`````````````````````````````````````````````
+ `````` ```````````` ```````````` ````+mddddddddddddmo`` `````````````````````````````````````````` 
+`` `` ``  `` `` ``  `` `` `` ``` `` `` -dddddddddddm+`` `` `` ``` `` `` ``  `` `` `` ``` `` `` ``  `
+ ``` `` `` ``  `` `` ``  `` `` `` ``  ```yddddddddm+` `` ``` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
+`` `` `````` `` `````` `` `` `````` `` ```omdddddmo```` `` `` ``` `` `` `````` `` `` ``` `` `` `````
+```````````````````````````````````````````oddddds``````````````````````````````````````````````````
+ ``` `` `` ``` `` `` `` ``` `` `` ``` `` `` sdddh` `` `` ``` `` `` ``` `` `` `` ``` `` `` ``` `` `` 
+`` `` ``  `` `` ``  `` `` `` ``` `` `` ``  ``hdd.``  `` `` `` ``  `` `` ``  `` `` ``  `` `` `` ``  `
+ ``` `` `` ``  `` `` ``  `` `` `` ``  `` `` `:N/`` `` `` ``` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
+"""
+print (A)
