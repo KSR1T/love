b1bbc5e81a09fcd6d44e26373e52fd3c08e783e4
KSR1T <68067743+KSR1T@users.noreply.github.com>
2021-01-21 08:01:23 +0300

    Update mylove.py

--- mylove.py
+++ mylove.py
@@ -1,54 +1,47 @@
-import time
-from time import sleep
-B = input ('Enter name love: ')
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-print ('ilove you '+B)
-time.sleep(3)
-A = """
- ``` `` `` ``  `` `` ``  `` `` `` ``  `` `` ``  `` `` `` ``` `` `` ``  `` `` ``  `` `` ...-::-::/++:
-`` `` `````` `` ````````` `` `````` `` ````````` ````.:+syyhdhyyo/-` `` `````` `` ``./oo/-.-////-.``
-``````````````````````````-:+oosssso/-````````````:shddddddddddddddds.````````````.:+sssyyysoo:`````
- ``` `` `` ``  `` `` `:ohmdhyyyyhddddddy+-` ````oddddddddddddddddddddms.` `` `-/ossssyyyyhss:``` `` 
-`` `` ``  `` `` ``  /hdhso+++++++hdddddddmy-` `hdddddddddddddddddddddddd.-:+ossssyyyo/:`+o- `` ``  `
- ``` `` `` ``  `` `sdho++++osyyhdddddddddddd/`sdddddddddddddddddddddddddmyssyyyyo/-``` `. ``` `` `` 
-``````````````````sdy+++shddddddddddddddddddm/mdddddddddddddddddddddddddddys+:.`````````````````````
-`````````````````:mdo+sddddddddddddddddddddddNmddddddddddddddddddddddddddm.`````````````````````````
- ``` `` `` ``  ``hdddddddddddddddddddddddddddmmddddddddddddddddddddddddddm-` ``  `` `` `` ``` `` `` 
-`` `` ``  `` `` .mdddddddddddddddddddddddddddddddddddddddddddddddddddddddm. `` `` `` ``` `` `` ``  `
- ``` `` `` ``  `:mdddddddddddddddddddddddddddddddddddddddddddddddddddddddd`` `` ``` `` `` ``` `` `` 
-````````````````:mdddddddddddddddddddddddddddddddddddddddddddddddddddddddo``````````````````````````
-`` `` `````` `` -mddddddddddddddddddddddddddddddddddddddddddddddddddddddd.```` `` ````````` ````````
- ``` `` `` ``  ``hdddddddddddddddddddddddddddddddddddddddddddddddddddddm- `` ``  `` `` `` ``` `` `` 
-`` `` ``  `` `` `:mdddddddddddddddddddddddddddddddddddddddddddddddddddd:``  `` `` `` ``` `` `` ``  `
- `````` ``````````omdddddddddddddddddddddddddddddddddddddddddddddddddd:`` `` ````````` ````````` `` 
-```````````````````+ddddddddddddddddddddddddddddddddddddddddddddddddd-``````````````````````````````
-`` `` `````` `` `-/+ymddddddddddddddddddddddddddddddddddddddddddddds``` `````` `` `` ``` `` `` `````
- ``  `.:+/.``.:+ssyyyhmmdddddddddddddddddddddddddddddddddddddddddd/``  `` `` ``  `` `` `` ``  `` `` 
-``./shmmmmdhyyyyhhhddho/oddddddddddddddddddddddddddddddddddddddms.`` `` ``  `` `` `` ``` `` `` ``  `
-:shdmmdhyyyhhdmdhy+:.`````+hmdddddddddddddddddddddddddddddddddh:````````````````````````````````````
-```+yyyhhddmmms-````````````:sddddddddddddddddddddddddddddddd+``````````````````````````````````````
-`` /hddmmmmmmm- `` ``` `` `` `.+hddddddddddddddddddddddddddo. ``` `` `` ``  `` `` `` ``` `` `` ``` `
- ```-+mmmmmmms``` `` ``  `` `` ``/dddddddddddddddddddddddo.` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
-`` ``/mmmds:``` `` ``` `` `` ``` ``odddddddddddddddddddh.` `` ``` `` `` `````` `` `` ``` `` `` ``` `
-`````hy+.```````````````````````````.ydddddddddddddddds`````````````````````````````````````````````
- `````` ```````````` ```````````` ````+mddddddddddddmo`` `````````````````````````````````````````` 
-`` `` ``  `` `` ``  `` `` `` ``` `` `` -dddddddddddm+`` `` `` ``` `` `` ``  `` `` `` ``` `` `` ``  `
- ``` `` `` ``  `` `` ``  `` `` `` ``  ```yddddddddm+` `` ``` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
-`` `` `````` `` `````` `` `` `````` `` ```omdddddmo```` `` `` ``` `` `` `````` `` `` ``` `` `` `````
-```````````````````````````````````````````oddddds``````````````````````````````````````````````````
- ``` `` `` ``` `` `` `` ``` `` `` ``` `` `` sdddh` `` `` ``` `` `` ``` `` `` `` ``` `` `` ``` `` `` 
-`` `` ``  `` `` ``  `` `` `` ``` `` `` ``  ``hdd.``  `` `` `` ``  `` `` ``  `` `` ``  `` `` `` ``  `
- ``` `` `` ``  `` `` ``  `` `` `` ``  `` `` `:N/`` `` `` ``` `` `` ``  `` `` ``  `` `` `` ``` `` `` 
-"""
-print (A)
+username = input("Enter username:")
+pasword = input("Enter Pasword:")
+Target = input("Enter Target:")
+r = requests.session()
+url = "https://www.instagram.com/accounts/login/ajax/"
+headers = {
+"accept":"*/*",
+"accept-encoding":"gzip, deflate,br",
+"accept-language": "ar,en-US;q=0.9,en;q=0.8",
+"content-length": "279",
+"content-type": "application/x-www-form-urlencoded",
+"origin": "https://www.instagram.com",
+"referer": "https://www.instagram.com/",
+"sec-fetch-dest":"empty",
+"sec-fetch-site":"same-origin",
+"user-agent":"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+"x-csrftoken": "lih2ypMfhzdqwMbm5jIILqxZDj4zLeCW",
+"x-ig-app-id": "936619743392459",
+"x-ig-www-claim": "hmac.AR1_p9SjMFQF73bcZgzygDgxb9yBZUn83e69xoDD2qpSdmtW",
+"x-instagram-ajax":"901e37113a69",
+"x-requested-with":"XMLHttpRequest"
+}
+data = {"username":username,"enc_password":"#PWD_INSTAGRAM_BROWSER:0:1589682409:"+pasword,"queryParams":"{}","optIntoOneTap":"false"}
+login = r.post(url,headers=headers,data=data,allow_redirects=True)
+if login.text.find("userId") >= 0 :
+    print('[√] Done Login:',username)
+    s = requests.get("https://instagram.com/"+Target+"/?__a=1")
+    idinsta =str(s.json()["graphql"]["user"]["id"])
+    print("[√] id Target:",idinsta)
+    data2 = {"source_name":"","reason_id":1,"frx_context":""}
+    r.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
+    for x in range(100000000):
+     spam = "https://www.instagram.com/users/"+idinsta+"/report/"
+     spamr = r.post(spam,data=data2)
+     if spamr.text.find("Your reports help keep our community free of spam."):
+          print("[√] Done Report spam:",Target)
+          time.sleep(60)
+    else:
+        print("[-] Error Report spam")
+        time.sleep(120)
+else:
+    if login.text.find("checkpoint_required"):
+     print("[-] Error..secure account !")
+    else:
+     print('[-] Error..worng Pasword')
+     
+   
